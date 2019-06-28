58f2b9fd35c15dd1f5d1fdfd14e68b8ee0507318
tastyloper <tastyloper@gmail.com>
2019-06-28 11:23:42 +0900

    feat: make add List

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { List } from 'src/app/core/interface/list.interface';
+
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  appUrl: string = environment.appUrl;
+  lists: List[];
+  addListOpen = true;
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {
+
   }
 
+  generateListSort() {
+    return this.lists.length ? Math.max(...this.lists.map(({ listSort }) => listSort)) : 0;
+  }
+
+  addList(input: HTMLInputElement) {
+    if (!input.value.trim()) { return; }
+    this.http.post<List>(this.appUrl + 'title/', {
+      title: input.value,
+      listSort: this.generateListSort()
+    }).subscribe(list => this.lists = [...this.lists, list]);
+    input.value = '';
+  }
 }
