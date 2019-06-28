a77b4530b60435215510cb03660822dfc8fea12c
progyu <rocket7101@gmail.com>
2019-06-28 12:20:42 +0900

    Header background change

--- src/app/components/trello/trello.component.ts
+++ src/app/components/trello/trello.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { BgColors } from 'src/app/core/type/bg-color';
 
 @Component({
   selector: 'app-trello',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrelloComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
 
+  colorBoolean: boolean = false;
+  colors: BgColors[] = ['#0079BF', '#D29034', '#4BBF6B', '#B03642'];
+  changeBgColor(color: BgColors) {
+    this.renderer.setStyle(document.body, 'background-color', color);
+    this.colorBoolean = !this.colorBoolean;
+  }
 }
