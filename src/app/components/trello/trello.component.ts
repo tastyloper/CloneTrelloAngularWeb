af5898ee1ef92f8bd55b6a1ee189a53a7088553b
progyu <rocket7101@gmail.com>
2019-06-28 13:31:53 +0900

    HttpRequest background chnage, delete background-color

--- src/app/components/trello/trello.component.ts
+++ src/app/components/trello/trello.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
 import { BgColors } from 'src/app/core/type/bg-color';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-trello',
@@ -7,16 +9,26 @@ import { BgColors } from 'src/app/core/type/bg-color';
   styleUrls: ['./trello.component.css']
 })
 export class TrelloComponent implements OnInit {
-
-  constructor(private renderer: Renderer2) { }
-
-  ngOnInit() {
-  }
-
+  appUrl: string = environment.appUrl;
   colorBoolean: boolean = false;
   colors: BgColors[] = ['#0079BF', '#D29034', '#4BBF6B', '#B03642'];
+
+  constructor(private renderer: Renderer2, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getBgColor();
+  }
+
+  getBgColor() {
+    this.http.get(this.appUrl + 'backgroundcolors/1')
+      .subscribe(backColor => this.renderer.setStyle(document.body, 'background-color', backColor['background_color']));
+  }
+
   changeBgColor(color: BgColors) {
-    this.renderer.setStyle(document.body, 'background-color', color);
     this.colorBoolean = !this.colorBoolean;
+    this.http.patch(this.appUrl + 'backgroundcolors/1', { background_color: color })
+      .subscribe(backColor =>
+        this.renderer.setStyle(document.body, 'background-color', backColor['background_color'])
+      );
   }
 }
